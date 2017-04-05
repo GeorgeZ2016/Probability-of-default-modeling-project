6af784706fd0d4d2df41819a78dd7afd67fec259
GeorgeZ2016 <1293573440@qq.com>
2017-04-05 13:47:31 -0400

    structure the data for machine learning algorithms and fit different model

--- structuring and modeling.py
+++ structuring and modeling.py
@@ -15,14 +15,22 @@ from sklearn.pipeline import Pipeline
 from sklearn.cross_validation import StratifiedKFold
 from sklearn.cross_validation import cross_val_score
 from sklearn.metrics import accuracy_score
-from sklearn.grid_search import GridSearchCV
-
-# Data
+from sklearn.metrics import confusion_matrix
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+import random
+from sklearn.utils import shuffle
+# EDA
 data=pd.read_csv('E:/project.csv')
+sum(data['loss_flag']==1)
 df=data.dropna()
+sum(df['loss_flag']==1)
+sum(df['loss_flag']==0)
+
 df=df.as_matrix()
 y=df[:, -1]
 x=df[:,:-1]
+
 # Get number of principle component:5
 stdsc = StandardScaler()
 x1= stdsc.fit_transform(x)
@@ -32,6 +40,9 @@ pca.explained_variance_ratio_
 
 ## first pipeline
 X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=0)
+sum(y_train==1)
+sum(y_test==1)
+
 pipe_lr = Pipeline([('scl', StandardScaler()),
 ('pca', PCA(n_components=1)),
 ('clf', LogisticRegression(random_state=1))])
@@ -50,6 +61,14 @@ print('CV accuracy: %.8f +/- %.8f' % (np.mean(scores),np.std(scores)))
 # modeling
 pipe_lr.fit(X_train, y_train)
 print('Test Accuracy: %.6f' % pipe_lr.score(X_test, y_test))
+#为什么说模型不好(Why the model is bad)
+pipe_lr.score(X_test[y_test==1], y_test[y_test==1])
+pipe_lr.score(X_test[y_test==0], y_test[y_test==0])
+
+
+# confusion matrix
+ypre=pipe_lr.predict(X_test)
+confmat = confusion_matrix(y_true=y_test, y_pred=ypre)
 
 ## Second pipeline
 x1=x[:,[0,2,3,4,5]]
@@ -57,16 +76,7 @@ X_train1, X_test1, y_train1, y_test1 = train_test_split(x1, y, test_size=0.3, ra
 pipe_lr1 = Pipeline([('scl', StandardScaler()),
 ('clf', LogisticRegression(random_state=0))])
 
-# Tuning hyperparameter.The solution is 0.0001
-param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
-param_grid = [{'clf__C': param_range}]
-gs = GridSearchCV(estimator=pipe_lr1,param_grid=param_grid,scoring='accuracy',
-cv=10,n_jobs=-1)
-gs = gs.fit(X_train1, y_train1)
-print(gs.best_score_)
-print(gs.best_params_)
-
-# single step, accuracy score
+# single step
 m=LogisticRegression(random_state=0)
 m.fit(X_train1, y_train1)
 y_pred = m.predict(X_test1)
@@ -75,8 +85,6 @@ print('Accuracy: %.8f' % accuracy_score(y_test1, y_pred))
 # K-fold CV
 kfold1 = StratifiedKFold(y=y_train1,n_folds=10,random_state=1)
 scores1=[]
-pipe_lr1 = Pipeline([('scl', StandardScaler()),
-('clf', LogisticRegression(C=10**(-5),random_state=0))])
 for k, (train, test) in enumerate(kfold1):
     pipe_lr1.fit(X_train1[train], y_train1[train])
     score1 = pipe_lr1.score(X_train1[test], y_train1[test])
@@ -90,5 +98,52 @@ print('CV accuracy: %.8f +/- %.8f' % (np.mean(scores),np.std(scores1)))
 pipe_lr1.fit(X_train1, y_train1)
 print('Test Accuracy: %.6f' % pipe_lr1.score(X_test1, y_test1))
 
+# Adaboost
+x_pca=X_train_pca[:,0:5]
+xtrada,xtestada,ytrada,ytestada=train_test_split(x_pca, y, test_size=0.3, random_state=0)
+m_ori=DecisionTreeClassifier(criterion='entropy',max_depth=5)
+ada = AdaBoostClassifier(base_estimator=m_ori,n_estimators=200,learning_rate=1,
+                         random_state=0)
+ada = ada.fit(xtrada, ytrada)
+ada_predict=ada.predict(xtestada)
+print('Test Accuracy: %.6f' % accuracy_score(ytestada, ada_predict))
+print('Test Accuracy: %.6f' % accuracy_score(ytestada[ytestada==0], ada_predict[ytestada==0]))
+print('Test Accuracy: %.6f' % accuracy_score(ytestada[ytestada==1], ada_predict[ytestada==1]))
 
+# Oversampling
+x_pca=X_train_pca[:,0:5]
+xtrpca,xtestpca,ytrpca,ytespca=train_test_split(x_pca, y, test_size=0.3, random_state=0)
+length=0.7*len(x)
+x_maj=xtrpca[ytrpca==0]
+x_min=xtrpca[ytrpca==1]
+y_maj,y_min=ytrpca[ytrpca==0],ytrpca[ytrpca==1]
+lx=[]
+ly=[]
+for i in range(int(length*0.7)):
+    r=random.randint(0,len(x_maj-1))
+    lx.append(list(x_maj[r]))
+    ly.append([y_maj[r]])
+for i in range(int(length*0.3)):
+    r=random.randint(0,len(x_min)-1)
+    lx.append(list(x_min[r]))
+    ly.append([y_min[r]])
+x3=np.array(lx)
+y3=np.array(ly)
+x3,y3=shuffle(x3,y3,random_state=0)
+# Model1:Logistic
+m3=LogisticRegression(random_state=0)
+m3.fit(x3, y3)
+y3_pred = m3.predict(xtestpca)
+print('Test Accuracy: %.6f' % accuracy_score(ytespca, y3_pred))
+print('Test Accuracy: %.6f' % accuracy_score(ytespca[ytespca==0], y3_pred[ytespca==0]))
+print('Test Accuracy: %.6f' % accuracy_score(ytespca[ytespca==1], y3_pred[ytespca==1]))
 
+# Model2:Adaboost
+m_ori=DecisionTreeClassifier(criterion='entropy',max_depth=5)
+ada = AdaBoostClassifier(base_estimator=m_ori,n_estimators=50,learning_rate=0.01,
+                         random_state=0)
+ada = ada.fit(x3, y3)
+ada_pred=ada.predict(xtestpca)
+print('Ada Test Accuracy: %.6f' % accuracy_score(ytespca, ada_pred))
+print('Ada Test Accuracy: %.6f' % accuracy_score(ytespca[ytespca==0], ada_pred[ytespca==0]))
+print('Ada Test Accuracy: %.6f' % accuracy_score(ytespca[ytespca==1], ada_pred[ytespca==1]))
